018c6109237fdf661457bd5f06ed0453971fc04d
fayohkorir <korirjfaith@gmail.com>
2023-11-07 03:54:56 -0800

    Write a function that reallocates a memory block using malloc and free
    Prototype: void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
    where ptr is a pointer to the memory previously allocated with a call to malloc: malloc(old_size)
    old_size is the size, in bytes, of the allocated space for ptr
    and new_size is the new size, in bytes of the new memory block
    The contents will be copied to the newly allocated space, in the range from the start of ptr up to the minimum of the old and new sizes
    If new_size > old_size, the “added” memory should not be initialized
    If new_size == old_size do not do anything and return ptr
    If ptr is NULL, then the call is equivalent to malloc(new_size), for all values of old_size and new_size
    If new_size is equal to zero, and ptr is not NULL, then the call is equivalent to free(ptr). Return NULL
    Don’t forget to free ptr when it makes sense

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previously allocated with malloc(old_size)
+ * @old_size: size of the allocated space for ptr
+ * @new_size: new size of the new memory block
+ *
+ * Return: pointer to the newly allocated memory block
+ *         NULL if new_size == 0 and ptr is not NULL
+ *         NULL if malloc fails
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *nptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		nptr = malloc(new_size);
+
+		if (nptr == NULL)
+			return (NULL);
+
+		return (nptr);
+	}
+	else
+	{
+		if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
+	}
+
+	nptr = malloc(new_size);
+
+	if (nptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size && i < new_size; i++)
+	{
+		nptr[i] = ((char *) ptr)[i];
+	}
+
+	free(ptr);
+	return (nptr);
+}
